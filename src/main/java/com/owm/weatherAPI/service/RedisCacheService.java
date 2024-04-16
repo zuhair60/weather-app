ca84f429b2da80f63acbd0dd0a6bf9071eebdd1f
Rana Zuahir <109857479+zuhair60@users.noreply.github.com>
2024-04-16 23:23:41 +0500

    Update RedisCacheService.java

--- src/main/java/com/owm/weatherAPI/service/RedisCacheService.java
+++ src/main/java/com/owm/weatherAPI/service/RedisCacheService.java
@@ -11,7 +11,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
-// A Java class implementing cache service using Redis for weather information storage and retrieval.
+
 
 @Service
 public class RedisCacheService implements CacheService {
@@ -22,36 +22,31 @@ public class RedisCacheService implements CacheService {
 
     private static final String WEATHER_CACHE = "weatherCache";  // Cache name
 
-// A Java method to retrieve weather information from Redis cache using Jackson for deserialization.
+
     @Override
     public WeatherInformation getWeatherFromCache(String key) {
         String cacheKey = WEATHER_CACHE + ":" + key;
         String serializedWeather = redisTemplate.opsForValue().get(cacheKey);
         if (serializedWeather != null) {
-            // Log the cache key for verification
             log.info("Cache key: {}", cacheKey);
-            // Deserialize weather information (using Jackson if needed)
-            ObjectMapper objectMapper = new ObjectMapper();  // Example using Jackson
+            ObjectMapper objectMapper = new ObjectMapper();  
             try {
                 return objectMapper.readValue(serializedWeather, WeatherInformation.class);
             } catch (JsonProcessingException e) {
-                // Handle deserialization exception
                 log.error("Error deserializing weather information from cache: {}", e.getMessage());
                 return null;
             }
         }
         return null;
     }
-
+    
     @Override
     public void cacheWeather(String key, WeatherInformation weatherInformation) {
-        // Serialize weather information (using Jackson if needed)
-        ObjectMapper objectMapper = new ObjectMapper(); // Example using Jackson
+        ObjectMapper objectMapper = new ObjectMapper(); 
         String serializedWeather = null;
         try {
             serializedWeather = objectMapper.writeValueAsString(weatherInformation);
         } catch (JsonProcessingException e) {
-            // Handle serialization exception
             e.printStackTrace();
         }
         if (serializedWeather != null) {
